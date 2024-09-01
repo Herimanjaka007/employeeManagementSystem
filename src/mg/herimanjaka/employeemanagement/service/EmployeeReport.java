76369b5a03d4c9f314048d46296d965e9d9fa23f
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-09-01 18:03:07 +0300

    add doc for all methods in Report service

--- src/mg/herimanjaka/employeemanagement/service/EmployeeReport.java
+++ src/mg/herimanjaka/employeemanagement/service/EmployeeReport.java
@@ -7,6 +7,14 @@ import java.util.stream.Collectors;
 import mg.herimanjaka.employeemanagement.model.Employee;
 
 public class EmployeeReport {
+    /**
+     * Calculates the average salary for each department from the given list of
+     * employees.
+     *
+     * @param employees the list of employees to process
+     * @return a Map where the keys are department names and the values are the
+     *         average salary of employees in that department
+     */
     public static Map<String, Double> averageSalaryByDepartment(List<Employee> employees) {
         return employees.stream()
                 .collect(Collectors.groupingBy(
@@ -14,6 +22,12 @@ public class EmployeeReport {
                         Collectors.averagingDouble(Employee::getSalary)));
     }
 
+    /**
+     * Counts the number of employees in each department.
+     *
+     * @param employees the list of employees to process
+     * @return a Map of department and the number of employees
+     */
     public static Map<String, Long> countEmployeesByDepartment(List<Employee> employees) {
         return employees.stream()
                 .collect(Collectors.groupingBy(
@@ -21,6 +35,12 @@ public class EmployeeReport {
                         Collectors.counting()));
     }
 
+    /**
+     * Calculates the total salary of all employees in the given list.
+     *
+     * @param employees the list of employees to process
+     * @return the total salary of all employees
+     */
     public static double totalSalary(List<Employee> employees) {
         return employees.stream()
                 .mapToDouble(Employee::getSalary)
